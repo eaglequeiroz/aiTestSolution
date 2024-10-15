286eac6e4c6fde6de9e3ee01c4858e3114a33ef9
eaglequeiroz <igorfqueirozz@gmail.com>
2024-10-15 17:05:30 -0300

    Initial commit

---
+++ src/main/java/com/burningeagle/aiTestSolution/AiTestSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.burningeagle.aiTestSolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiTestSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiTestSolutionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/burningeagle/aiTestSolution/AiTestSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burningeagle.aiTestSolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiTestSolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
